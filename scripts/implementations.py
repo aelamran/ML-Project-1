8757cfe2276c258896cd149c615df8b7e7419904
Ayyoub EL AMRANI <ayyoub.elamrani4@gmail.com>
2018-10-29 07:15:08 +0100

    final commit

--- scripts/implementations.py
+++ scripts/implementations.py
@@ -1,6 +1,6 @@
-import numpy as np
+from scripts.proj1_helpers import *
 import matplotlib.pyplot as plt
-    
+
 
 def least_squares_GD(y, tx, initial_w, max_iters, gamma):
     """
@@ -39,7 +39,7 @@ def least_squares_SGD(y, tx, initial_w, max_iters, gamma):
     w = initial_w
     for n_iter in range(max_iters):
         for y_batch, tx_batch in batch_iter(y, tx, batch_size=1, num_batches=1):
-            grad, _ = compute_stoch_gradient(y_batch, tx_batch, w)
+            grad, _ = compute_stochastic_gradient(y_batch, tx_batch, w)
             w = w - (gamma * grad)
             loss = compute_loss(y, tx, w)
     return w, loss
@@ -193,7 +193,7 @@ def compute_gradient(y, tx, w):
     return grad, loss
 
 
-def compute_stoch_gradient(y, tx, w):
+def compute_stochastic_gradient(y, tx, w):
     """
 
     :param y: the output data
@@ -257,3 +257,274 @@ def batch_iter(y, tx, batch_size, num_batches=1, shuffle=True):
         end_index = min((batch_num + 1) * batch_size, data_size)
         if start_index != end_index:
             yield shuffled_y[start_index:end_index], shuffled_tx[start_index:end_index]
+
+
+def compute_accuracy(w_train, x, y):
+    """
+
+    :param w_train: The optimal weights after the training
+    :param x: The x on which we wish to proceed prediction
+    :param y: the actual output with which the comparaison is going to take place
+    :return: the accuracy score
+    """
+    '''
+    Calculates the accuracy by comparing the predictions with given test data.
+    '''
+    pred = predict_labels(w_train, x)
+    N = len(pred)
+    count = 0.0
+    for i in range(len(pred)):
+        if pred[i] == y[i]:
+            count += 1
+    return count/N
+
+
+def split_data(tx, ty, ratio, seed=1):
+    """
+
+    :param tx: The input data
+    :param ty: the output data
+    :param ratio: the ratio used for the split
+    :param seed: the seed generating randomness
+    :return: the training and testing sets with its inputs and outputs
+    """
+    '''
+    Split the training data by ratio.
+    '''
+    np.random.seed(seed)
+    split_idxs = [i for i in range(len(tx))]
+
+    # Shuffle the indicies randomly
+    np.random.shuffle(split_idxs)
+    tx_shuffled = tx[split_idxs]
+    ty_shuffled = ty[split_idxs]
+
+    # Split by ratio
+    split_pos = int(len(tx) * ratio)
+    x_train = tx_shuffled[:split_pos]
+    x_test = tx_shuffled[split_pos:]
+    y_train = ty_shuffled[:split_pos]
+    y_test = ty_shuffled[split_pos:]
+
+    return x_train, y_train, x_test, y_test
+
+
+def build_poly(x, degree):
+    """
+
+    :param x: The matrix on which we wish to build the polynomial degree
+    :param degree: the polynomial degree built
+    :return: a matrix having the polynomial degree built
+    """
+    '''
+    Builds a polynomial of the given degree and appends it to the given matrix.
+    '''
+    x_ret = x
+    for i in range(2, degree+1):
+        x_ret = np.c_[x_ret, np.power(x, i)]
+    return x_ret
+
+
+def normalize(x):
+    """
+
+    :param x: The data we want to standardize
+    :return: the data standardized
+    """
+    '''
+    Standardizes the matrix by subtracting mean of each column and then dividing by standard deviation.
+    '''
+    res = x.copy()
+    for i in range(0, res.shape[1]):
+        if i == 22:
+            continue
+
+        # Calculate mean and standard deviation without including NaN values
+        mean = np.nanmean(res[:, i])
+        std = np.nanstd(res[:, i])
+        median = np.nanmedian(res[:, i])
+
+
+        # Change mean and standard deviation if column has all NaN values
+        if np.isnan(median):
+            median = 0
+        if np.isnan(std) or std == 0:
+            std = 1
+
+        # Replaces NaN values with mean and divides by standard deviation
+        for j in range(len(res[:, i])):
+            if np.isnan(res[j][i]):
+                res[j][i] = median
+                # print(median)
+                res[j][i] -= mean
+            else:
+                res[j][i] -= mean
+            res[j][i] /= std
+    return res
+
+
+def replace_999_with_nan(x):
+    """
+
+    :param x: data with 999 values
+    :return: data having "999" replaced with "nan"
+    """
+    '''
+    Replaces -999 (undefined values) with NaN.
+    '''
+    res = x.copy()
+    res[res == -999.0] = np.nan
+    return res
+
+
+def get_buckets(x):
+    """
+
+    :param x: Our dataset
+    :return: different buckets from the dataset based on some categorical values
+    """
+    '''
+    Splits the dataset into 8 buckets.
+    Based on 4 values (0, 1, 2, 3) of PRI_jet_num and 2 values (defined or -999) of DER_mass_MMC.
+    '''
+    result = []
+    for i in range(0, 4):
+        # Get all rows where PRI_jet_num equals i
+        x_jet_feature = x[x[:, 22] == i]
+
+        # Get all rows where DER_mass_MMC defined and undefined
+        xi_defined = x_jet_feature[x_jet_feature[:, 0] != -999.0]
+        xi_undefined = x_jet_feature[x_jet_feature[:, 0] == -999.0]
+
+        result.append(xi_defined)
+        result.append(xi_undefined)
+    return result
+
+
+def get_id_buckets(x):
+    """
+
+    :param x: Our dataset
+    :return: getting the ids of the buckets generated with the methode get_buckets
+    """
+    '''
+    Splits the set of ids into 8 buckets as above.
+    Used for sorting the predictions based on event id.
+    '''
+    id_buckets = []
+    for i in range(0, 4):
+        x_jet_feature = x[x[:, 1] == i]
+        xi_defined = x_jet_feature[x_jet_feature[:, -1] != -999.0]
+        xi_undefined = x_jet_feature[x_jet_feature[:, -1] == -999.0]
+        id_buckets.append(xi_defined)
+        id_buckets.append(xi_undefined)
+
+    return id_buckets
+
+
+def build_k_indices(y, k_fold, seed):
+    """
+
+    :param y: Our ouput y
+    :param k_fold: the number of folds for the cross validation procedure
+    :param seed: the random parameter
+    :return: array of indices shuffled for the purpose of k-fold cross validation
+    """
+    """build k indices for k-fold."""
+    num_row = y.shape[0]
+    interval = int(num_row / k_fold)
+    np.random.seed(seed)
+    indices = np.random.permutation(num_row)
+    k_indices = [indices[k * interval: (k + 1) * interval] for k in range(k_fold)]
+    return np.array(k_indices)
+
+
+def cross_validation(y, x, k_indices, k, lambda_, degree):
+    """
+
+    :param y: output data
+    :param x: input data
+    :param k_indices: indices used for cross validation
+    :param k: the kth set is the test cross validation set
+    :param lambda_: penalising lambda of ridge regression
+    :param degree: polynomial degree of feature engineering
+    :return: the training/testing loss considering the kth set as the test set
+    """
+    """return the loss of ridge regression."""
+    # get k'th subgroup in test, others in train
+    test_indices = k_indices[k]
+    train_indices = k_indices[~(np.arange(k_indices.shape[0]) == k)]
+    train_indices = train_indices.reshape(-1)
+    y_te = y[test_indices]
+    y_tr = y[train_indices]
+    x_te = x[test_indices]
+    x_tr = x[train_indices]
+    # form data with polynomial degree
+    tx_tr = build_poly(x_tr, degree)
+    tx_te = build_poly(x_te, degree)
+    # ridge regression
+    w, _ = ridge_regression(y_tr, tx_tr, lambda_)
+    # calculate the loss for train and test data
+    loss_tr = np.sqrt(2 * compute_loss(y_tr, tx_tr, w))
+    loss_te = np.sqrt(2 * compute_loss(y_te, tx_te, w))
+    return loss_tr, loss_te, w
+
+
+def best_degree_selection(y, x, degrees, k_fold, lambdas, seed=1):
+    """
+
+    :param y: output data
+    :param x: input data
+    :param degrees: different polynomial degrees on which we wish to get the best one
+    :param k_fold: number of folds of the cross validation procedure
+    :param lambdas: different penalizing lambdas on which we wish to get the best
+    :param seed: random factor
+    :return: the best polynomial degree based on least mse of the best lambda
+    """
+    # split data in k fold
+    k_indices = build_k_indices(y, k_fold, seed)
+
+    # for each degree, we compute the best lambdas and the associated rmse
+    best_lambdas = []
+    best_rmses = []
+    # vary degree
+    for degree in degrees:
+        # cross validation
+        rmse_te = []
+        for lambda_ in lambdas:
+            rmse_te_tmp = []
+            for k in range(k_fold):
+                _, loss_te, _ = cross_validation(y, x, k_indices, k, lambda_, degree)
+                rmse_te_tmp.append(loss_te)
+            rmse_te.append(np.mean(rmse_te_tmp))
+
+        ind_lambda_opt = np.argmin(rmse_te)
+
+        best_lambdas.append(lambdas[ind_lambda_opt])
+        best_rmses.append(rmse_te[ind_lambda_opt])
+
+    ind_best_degree = np.argmin(best_rmses)
+    print("best lambdas : " + str(lambdas[ind_lambda_opt]))
+    print("best degree "+str(degrees[ind_best_degree]))
+
+    return degrees[ind_best_degree]
+
+def cross_validation_visualization(lambds, mse_tr, mse_te,b):
+    """
+
+    :param lambds: Different penalizing lambdas
+    :param mse_tr: mean squared error of the training set
+    :param mse_te: mean squared error of the testing set
+    :param b: an integer referring to the bucket on which we are cross validating
+    :return: cross validation figure
+    """
+    """visualization the curves of mse_tr and mse_te."""
+    plt.semilogx(lambds, mse_tr, marker=".", color='b', label='train error')
+    plt.semilogx(lambds, mse_te, marker=".", color='r', label='test error')
+    plt.xlabel("lambda")
+    plt.ylabel("rmse")
+    plt.title("cross validation")
+    plt.legend(loc=2)
+    plt.grid(True)
+    plt.savefig("cross_validation for bucket "+str(b))
+
--- scripts/run.py
+++ scripts/run.py
@@ -1,13 +1,122 @@
-from scripts.proj1_helpers import *
 from scripts.implementations import *
 
 # Constants for importing training and testing data
 TRAINING_DATA = '/home/ayyoubelamrani/Documents/ML/ML_course/projects/project1/all/train.csv'
 TEST_DATA = '/home/ayyoubelamrani/Documents/ML/ML_course/projects/project1/all/test.csv'
 
+# Ridge regression parameters
+SPLIT_RATIO = 0.80
+DEGREE = 11
+LAMBDA = 0.8357142857142857
+
+print("Loading data from train = " + TRAINING_DATA + " test = " + TEST_DATA)
+print("------------------LOADING DATA------------------")
+
 # Load csv training and testing data
-ty, tx, ids_train = load_csv_data(TRAINING_DATA, sub_sample=False)
-#fy, fx, ids_test = load_csv_data(TEST_DATA, sub_sample=True)
+training_y, training_x, ids_train = load_csv_data(TRAINING_DATA, sub_sample=False)
+submission_y, submission_x, ids_test = load_csv_data(TEST_DATA, sub_sample=False)
 
-#print(load_csv_data(TRAINING_DATA, sub_sample=True))
+print("------------------DATA LOADED------------------.")
 
+# Backup of the imported training and testing data
+training_x_backup = training_x.copy()
+training_y_backup = training_y.copy()
+submission_x_backup = submission_x.copy()
+
+print("------------------PRECESSING------------------")
+
+# Split data into 80-20 and use 80 for training and 20 to check model accuracy
+x_train, y_train, x_test, y_test = split_data(training_x_backup.copy(), training_y_backup.copy(), SPLIT_RATIO, seed=1)
+
+# Split final test data to make predictions on into buckets
+submission_x_train = submission_x_backup.copy()
+submission_x_buckets = get_buckets(submission_x_train)
+
+# Append y values as column to later divide y into buckets corresponding with x values
+x_train = np.column_stack((x_train, y_train))
+x_test = np.column_stack((x_test, y_test))
+
+# Split training x into buckets
+buckets = get_buckets(x_train)
+
+# Split testing y into buckets corresponding to x values
+y_buckets = []
+for i in range(len(buckets)):
+    y_buckets.append(buckets[i][:, -1])
+    buckets[i] = np.delete(buckets[i], -1, 1)
+
+# Split testing x into buckets
+test_buckets = get_buckets(x_test)
+
+# Split testing y into buckets corresponding to x values
+test_y_buckets = []
+for i in range(len(test_buckets)):
+    test_y_buckets.append(test_buckets[i][:, -1])
+    test_buckets[i] = np.delete(test_buckets[i], -1, 1)
+
+# Standardize columns for each bucket
+for b in range(len(buckets)):
+    buckets[b] = normalize(buckets[b])
+    test_buckets[b] = normalize(test_buckets[b])
+    submission_x_buckets[b] = normalize(submission_x_buckets[b])
+
+# Build polynomial of given degree for each bucket
+for b in range(len(buckets)):
+    buckets[b] = build_poly(buckets[b], DEGREE)
+    test_buckets[b] = build_poly(test_buckets[b], DEGREE)
+    submission_x_buckets[b] = build_poly(submission_x_buckets[b], DEGREE)
+
+# Add column of ones for intercept for each bucket
+for b in range(len(buckets)):
+    buckets[b] = np.column_stack((np.ones((buckets[b].shape[0], 1)), buckets[b]))
+    test_buckets[b] = np.column_stack((np.ones((test_buckets[b].shape[0], 1)), test_buckets[b]))
+    submission_x_buckets[b] = np.column_stack((np.ones((submission_x_buckets[b].shape[0], 1)), submission_x_buckets[b]))
+
+print("------------------TRAINING OF RIDGE REGRESSION------------------")
+
+# Calculate weights for each bucket separately
+weights = []
+for i in range(len(buckets)):
+    w_rr, loss_rr = ridge_regression(y_buckets[i], buckets[i], LAMBDA)
+    weights.append(w_rr)
+
+# Compare predictions for each bucket using its corresponding weights found earlier
+correct_predictions = 0
+len_data = 0
+for i in range(len(buckets)):
+    rr_accuracy = compute_accuracy(weights[i], test_buckets[i], test_y_buckets[i])
+    correct_predictions += (rr_accuracy * len(test_buckets[i]))
+    len_data += len(test_buckets[i])
+
+print("------------------TRAINING COMPLETED------------------")
+total_accuracy = correct_predictions / len_data
+print("Total Accuracy = " + str(total_accuracy) + " Degree = " + str(DEGREE) + " Lambda = " + str(LAMBDA))
+
+
+# Create new array with Id, PRI_jet_num, and DER_mass_MMC for reordering predictions
+ids_array = ids_test
+pri_jet_num_feature = submission_x_backup[:, 22]
+der_mass_mmc_col_feature = submission_x_backup[:, 0]
+ids_array = np.column_stack((ids_array, pri_jet_num_feature))
+ids_array = np.column_stack((ids_array, der_mass_mmc_col_feature))
+
+# Divide Id into 8 buckets similar to input data
+id_buckets = get_id_buckets(ids_array)
+
+# Make predictions for each bucket using weights calculated by training on each bucket
+submission = predict_labels(weights[0], submission_x_buckets[0])
+submission = np.column_stack((submission, id_buckets[0]))
+for i in range(1, len(weights)):
+    predictions = predict_labels(weights[i], submission_x_buckets[i])
+    predictions = np.column_stack((predictions, id_buckets[i]))
+    submission = np.concatenate((submission, predictions))
+
+# Sort predictions based on Id
+submission = submission[submission[:, 1].argsort()]
+
+# Select only prediction values
+submission = submission[:, 0]
+
+# Create output file containing predictions
+create_csv_submission(ids_test, submission, "output.csv")
+print("Created output.csv with shape = " + str(submission.shape))
